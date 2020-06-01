c019522b34de705c8e8e63df02235ba62ed204a0
M-HARD <mumusab268000@gmail.com>
2020-06-01 07:48:00 +0200

    first commit

---
+++ build/js/app.js
@@ -0,0 +1,70 @@
+new Vue({
+    el: '#app',
+    data: {
+        text:"<h1 class='text-6xl font-bold text-center text-green-300'>Hello Musab!!</h1>",
+        recentwork:[
+            {
+                id:0,
+                name:"Meraki UI",
+                imgPath:"build/img/avatar.99ed4258.svg",
+                hoverText:"Beautiful Tailwind css components that support RTL out of the box & fully responsive.",
+                visitUrl:"https://nothingworks.netlify.com/"
+            },
+            {
+                id:0,
+                name:"Nothing works",
+                imgPath:"build/img/avatar.99ed4258.svg",
+                hoverText:"My personal blog, I write about our industry from time to time.",
+                visitUrl:"https://nothingworks.netlify.com/"
+            },
+            {
+                id:0,
+                name:"Meraki UI",
+                imgPath:"build/img/avatar.99ed4258.svg",
+                hoverText:"Beautiful Tailwind css components that support RTL out of the box & fully responsive.",
+                visitUrl:"https://nothingworks.netlify.com/"
+            },
+            {
+                id:0,
+                name:"Meraki UI",
+                imgPath:"build/img/avatar.99ed4258.svg",
+                hoverText:"Beautiful Tailwind css components that support RTL out of the box & fully responsive.",
+                visitUrl:"https://nothingworks.netlify.com/"
+            },
+            {
+                id:0,
+                name:"Meraki UI",
+                imgPath:"build/img/avatar.99ed4258.svg",
+                hoverText:"Beautiful Tailwind css components that support RTL out of the box & fully responsive.",
+                visitUrl:"https://nothingworks.netlify.com/"
+            },
+            {
+                id:0,
+                name:"Meraki UI",
+                imgPath:"build/img/avatar.99ed4258.svg",
+                hoverText:"Beautiful Tailwind css components that support RTL out of the box & fully responsive.",
+                visitUrl:"https://nothingworks.netlify.com/"
+            },
+        ],
+    },
+    methods: {
+        
+    },
+    computed: {
+        
+    },
+});
+
+
+
+
+/*
+
+
+
+<svg data-v-5bb76676="" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="fill-current text-indigo-600 w-16"><path data-v-5bb76676="" d="M5.0333 14.8284L6.44751 16.2426L10.6902 12L6.44751 7.75733L5.0333 9.17155L7.86172 12L5.0333 14.8284Z" fill="currentColor"></path><path data-v-5bb76676="" d="M15 14H11V16H15V14Z" fill="currentColor"></path><path data-v-5bb76676="" fill-rule="evenodd" clip-rule="evenodd" d="M2 2C0.895431 2 0 2.89543 0 4V20C0 21.1046 0.89543 22 2 22H22C23.1046 22 24 21.1046 24 20V4C24 2.89543 23.1046 2 22 2H2ZM22 4H2L2 20H22V4Z" fill="currentColor"></path></svg>
+<svg data-v-5bb76676="" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="fill-current text-indigo-600 w-16"><path data-v-5bb76676="" fill-rule="evenodd" clip-rule="evenodd" d="M11 17H4C2.34315 17 1 15.6569 1 14V6C1 4.34315 2.34315 3 4 3H20C21.6569 3 23 4.34315 23 6V14C23 15.6569 21.6569 17 20 17H13V19H16C16.5523 19 17 19.4477 17 20C17 20.5523 16.5523 21 16 21H8C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19H11V17ZM4 5H20C20.5523 5 21 5.44772 21 6V14C21 14.5523 20.5523 15 20 15H4C3.44772 15 3 14.5523 3 14V6C3 5.44772 3.44772 5 4 5Z" fill="currentColor"></path></svg>
+<svg data-v-5bb76676="" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="fill-current text-indigo-600 w-16"><path data-v-5bb76676="" fill-rule="evenodd" clip-rule="evenodd" d="M12.0122 5.57169L10.9252 4.48469C8.77734 2.33681 5.29493 2.33681 3.14705 4.48469C0.999162 6.63258 0.999162 10.115 3.14705 12.2629L11.9859 21.1017L11.9877 21.0999L12.014 21.1262L20.8528 12.2874C23.0007 10.1395 23.0007 6.65711 20.8528 4.50923C18.705 2.36134 15.2226 2.36134 13.0747 4.50923L12.0122 5.57169ZM11.9877 18.2715L16.9239 13.3352L18.3747 11.9342L18.3762 11.9356L19.4386 10.8732C20.8055 9.50635 20.8055 7.29028 19.4386 5.92344C18.0718 4.55661 15.8557 4.55661 14.4889 5.92344L12.0133 8.39904L12.006 8.3918L12.005 8.39287L9.51101 5.89891C8.14417 4.53207 5.92809 4.53207 4.56126 5.89891C3.19442 7.26574 3.19442 9.48182 4.56126 10.8487L7.10068 13.3881L7.10248 13.3863L11.9877 18.2715Z" fill="currentColor"></path></svg>
+
+
+*/
